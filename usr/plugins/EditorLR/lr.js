7bbc6d74afa234ebd77d578337607b5902add89c
hongweipeng <961365124@qq.com>
2015-12-10 13:54:06 +0800

    添加三种编辑模式

--- usr/plugins/EditorLR/lr.js
+++ usr/plugins/EditorLR/lr.js
@@ -6,26 +6,22 @@ function prettify() {
     prettyPrint();
 }
 
-// $(function() {
-//     /*Show wmd-preview DOM*/
-//     $('.wmd-edittab').remove();
-//     $('#wmd-preview').removeClass('wmd-hidetab');
-//     // setInterval("$('#wmd-preview').css('height', (parseInt($('#text').height()) - 5)+'px');", 500);
-    
-// });
-
 //滚动条添加监听 同步两边的滚动条
 $(document).ready(function(){
     var syn = true;//防止两个滚动条相互监听而引起无穷无尽的死锁问题
+    var is_syn_time = true;//是否实时
     function syn_scroll(source, target) {
         source.scroll(function(){
             if(!syn) return;
             syn = false;
-            var source_scroll_height = source.scrollTop();
-            var source_area_height = source.height();
-            var bl = source_scroll_height / source_area_height;//滚动条占的百分比
-            var target_area_height = target.height();
-            target.scrollTop(Math.ceil(bl * target_area_height));
+            var source_scroll_top = source.scrollTop();
+            var source_scroll_height = source.get(0).scrollHeight;
+            var source_offset_height = source.get(0).offsetHeight;
+
+            var target_offset_height = target.get(0).offsetHeight;
+            var target_scroll_height = target.get(0).scrollHeight;
+
+            target.scrollTop(source_scroll_top * (target_scroll_height - target_offset_height) / (source_scroll_height - source_offset_height));
             setTimeout(function(){
                 syn = true;
             },20);
@@ -44,6 +40,51 @@ $(document).ready(function(){
     });
     textArea.resize();//update resize
 
-    setInterval("prettify()", 500);//代码高亮
+    setInterval(function() {
+        if(!is_syn_time)
+            return;
+        prettify();
+    }, 500);//代码高亮
+
+    //添加编辑 实时 预览工具栏
+    var div = $('<div class="wmd-editlrtab"></div>');
+    var edit_mode = $('<a href="#">编辑</a>');
+    var both_mode = $('<a href="#">实时</a>');
+    var show_mode = $('<a href="#">浏览</a>');
+    div.append(edit_mode).append(both_mode).append(show_mode);
+    $('#wmd-button-bar').prepend(div);
+    function edit_change(obj, left, right, mode) {
+        obj.click(function() {  
+            if(obj.hasClass("active")) return;
+
+            is_syn_time = mode;
+
+            $('.wmd-editlrtab a').removeClass("active");
+            if(left < 15) {
+                textArea.hide();
+            } else {
+                textArea.show();
+            }
+            if(right < 15) {
+                wmd_preview.hide();
+            }else {
+                wmd_preview.show();
+            }
+
+            textArea.animate({width:left + '%'}, 200);
+            if(typeof right == 'string') {
+                wmd_preview.animate({width:($('#wmd-editarea').width()/ $(window).width()) * 100 + '%'}, 200);
+            } else {
+                wmd_preview.animate({width:right + '%'}, 200);
+            }
+            
+            obj.addClass('active');
+        });
+    }
+    edit_change(edit_mode, 100, 10,false);
+    edit_change(both_mode, 47, 47, true);
+    edit_change(show_mode, 10, 'full',false);
+    both_mode.addClass('active');
+
 });
 
