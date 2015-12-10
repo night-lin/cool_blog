07a39b38972b884c2cf58cdabc2f52485171306c
hongweipeng <961365124@qq.com>
2015-12-10 15:52:23 +0800

    优化编辑器

--- usr/plugins/EditorLR/lr.js
+++ usr/plugins/EditorLR/lr.js
@@ -10,6 +10,8 @@ function prettify() {
 $(document).ready(function(){
     var syn = true;//防止两个滚动条相互监听而引起无穷无尽的死锁问题
     var is_syn_time = true;//是否实时
+    var mode = 1;//当前编辑模式，默认为实时
+    var is_process = false;//正在编辑器模式切换中
     function syn_scroll(source, target) {
         source.scroll(function(){
             if(!syn) return;
@@ -34,56 +36,103 @@ $(document).ready(function(){
 
     $('.wmd-edittab').remove();
     wmd_preview.removeClass('wmd-hidetab');
-
+    var textArea_width = textArea.width();
     textArea.resize(function() {
+        if(is_process)
+            return;
         wmd_preview.outerHeight(textArea.outerHeight());
+        $('.wmd-editlrtab a').eq(mode).click();
     });
     textArea.resize();//update resize
 
     setInterval(function() {
-        if(!is_syn_time)
+        if(!is_syn_time)//如果不是实时模式，不需要再对代码渲染
             return;
         prettify();
     }, 500);//代码高亮
 
     //添加编辑 实时 预览工具栏
     var div = $('<div class="wmd-editlrtab"></div>');
-    var edit_mode = $('<a href="#">编辑</a>');
-    var both_mode = $('<a href="#">实时</a>');
-    var show_mode = $('<a href="#">浏览</a>');
+    var edit_mode = $('<a href="javascript:void(0);">编辑</a>');
+    var both_mode = $('<a href="javascript:void(0);">实时</a>');
+    var show_mode = $('<a href="javascript:void(0);">浏览</a>');
     div.append(edit_mode).append(both_mode).append(show_mode);
     $('#wmd-button-bar').prepend(div);
-    function edit_change(obj, left, right, mode) {
-        obj.click(function() {  
-            if(obj.hasClass("active")) return;
-
-            is_syn_time = mode;
-
-            $('.wmd-editlrtab a').removeClass("active");
-            if(left < 15) {
-                textArea.hide();
-            } else {
-                textArea.show();
-            }
-            if(right < 15) {
-                wmd_preview.hide();
-            }else {
-                wmd_preview.show();
-            }
-
-            textArea.animate({width:left + '%'}, 200);
-            if(typeof right == 'string') {
-                wmd_preview.animate({width:($('#wmd-editarea').width()/ $(window).width()) * 100 + '%'}, 200);
-            } else {
-                wmd_preview.animate({width:right + '%'}, 200);
-            }
-            
-            obj.addClass('active');
-        });
+    function obj_show(obj) {
+        obj.removeClass('lr-edit-hide').addClass('lr-edit-show');
     }
-    edit_change(edit_mode, 100, 10,false);
-    edit_change(both_mode, 47, 47, true);
-    edit_change(show_mode, 10, 'full',false);
+    function obj_hide(obj) {
+        obj.removeClass('lr-edit-show').addClass('lr-edit-hide');
+    }
+    //编辑器模式 0 编辑模式 1 实时模式 2 预览模式
+    function edit_change(_m) {
+        mode = _m;
+        var left, right;
+        is_process = true;
+        if(mode == 0) {
+            is_syn_time = false;
+            left = 100;
+            right = 10;
+        }else if(mode == 1) {
+            is_syn_time = true;
+            if($('body').hasClass('fullscreen')) {
+                left = right = 50;
+            }else {
+                left = right = 47;
+            }
+        }else if(mode == 2) {
+            is_syn_time = false;
+
+            if($('body').hasClass('fullscreen')) {
+                left = 10;
+                right = 100;
+            }else {
+                left = 0;
+                right = 'full';
+            }
+        }
+
+
+        $('.wmd-editlrtab a').removeClass("active");
+        
+        if(left < 15) {
+            if($('body').hasClass('fullscreen')) {
+                obj_hide(textArea);
+            }else {
+                textArea.hide();
+            }
+        } else {
+            obj_show(textArea);
+        }
+        if(right < 15) {
+            obj_hide(wmd_preview);
+        }else {
+            obj_show(wmd_preview);
+        }
+
+        textArea.animate({width:left + '%'}, 200);
+        if(typeof right == 'string') {
+            wmd_preview.animate({width:($('#wmd-editarea').width()/ $(window).width()) * 100 + '%'}, 200);
+        } else {
+            wmd_preview.animate({width:right + '%'}, 200);
+        }
+
+        $('.wmd-editlrtab a').eq(mode).addClass('active');
+
+        setTimeout(function(){
+                is_process = false;
+            },500);
+    }
+    edit_mode.click(function() {
+        edit_change(0);
+    });
+    both_mode.click(function() {
+        edit_change(1);
+    });
+    show_mode.click(function() {
+        edit_change(2);
+    });
+
     both_mode.addClass('active');
 
 });
