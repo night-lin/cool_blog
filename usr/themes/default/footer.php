103fbdc7e66ae2686f43cc981fdd04a90b0e4dc7
hongweipeng <961365124@qq.com>
2015-12-27 22:20:45 +0800

    版权信息

--- usr/themes/default/footer.php
+++ usr/themes/default/footer.php
@@ -6,7 +6,7 @@
 
 <footer id="footer" role="contentinfo">
     &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
-    <?php _e('由 <a href="http://www.typecho.org">Typecho</a> 强力驱动'); ?>.
+    <?php _e('All Rights Reserved. 版权所有'); ?>.
     <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1257015934'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1257015934%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
 </footer><!-- end #footer -->
 
