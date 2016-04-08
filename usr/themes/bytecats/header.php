30d4162f510538986f159381b81edd0624799590
Tony <1025934610@qq.com>
2016-04-08 10:47:00 +0800

    change header
    4.8

--- usr/themes/bytecats/header.php
+++ usr/themes/bytecats/header.php
@@ -13,7 +13,7 @@
         'search'    =>  _t('包含关键字 %s 的文章'),
         'tag'       =>  _t('标签 %s 下的文章'),
         'author'    =>  _t('%s 发布的文章')
-    ), '', ' | '); ?><?php $this->options->title(); ?><?php if($this->is('index')){echo " | 专注WEB前端开发与WordPress技术分析交流"; }?></title>
+    ), '', ' | '); ?><?php $this->options->title(); ?><?php if($this->is('index')){echo " | 学习 思考 分享"; }?></title>
 
 <!-- 使用url函数转换相关路径 -->
 <link rel="stylesheet" href="<?php $this->options->themeUrl('style.css'); ?>">
